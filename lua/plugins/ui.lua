bb1b99cf34eba4ba67afbab234119cca43e5e8d1
ao jia <ao_jia@outlook.com>
2024-11-17 21:04:14 +0800

    plugins[ui]: winbar for Neovim.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,4 +4,12 @@ return {
     "nvim-tree/nvim-tree.lua",
     opts = require("configs.ui").nvim_tree,
   },
+  { -- Breadcrumbs
+    "Bekaboo/dropbar.nvim",
+    lazy = false,
+    -- Optional, but required for fuzzy finder support
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+    },
+  },
 }
