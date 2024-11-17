44ba11b6cf8fb1edc9122b58bcbedf8913768291
ao jia <ao_jia@outlook.com>
2024-11-17 20:15:28 +0800

    statusline: init.

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,54 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+  theme = "catppuccin",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.ui = {
+  statusline = {
+    order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "encoding", "eol", "lsp", "cwd", "cursor" },
+    modules = {
+      encoding = function()
+        if vim.list_contains({ "help" }, vim.bo.filetype) then
+          return ""
+        end
+        local encoding = vim.bo.fileencoding:upper()
+        if encoding == "" then
+          return ""
+        end
+        return " %*" .. encoding .. " " -- "%*" is used to clear highlight group (use default highlight)
+      end,
+      eol = function()
+        if vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "terminal" then
+          return ""
+        end
+        if vim.list_contains({ "help", "terminal", "NvimTree", "nvdash" }, vim.bo.filetype) then
+          return ""
+        end
+        -- LF, CRLF or CR
+        local file_format = vim.bo.fileformat
+        local eol
+        if file_format == "unix" then
+          eol = "LF"
+        elseif file_format == "dos" then
+          eol = "CRLF"
+        elseif file_format == "mac" then
+          eol = "CR"
+        end
+        return " %*" .. eol .. " " -- "%*" is used to clear highlight group (use default highlight)
+      end,
+    },
+  },
 }
 
 return M
