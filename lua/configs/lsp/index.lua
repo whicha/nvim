03deed77e30a3f8ae55a7c4aa1e3784a0dca87bb
ao jia <ao_jia@outlook.com>
2024-11-17 19:45:37 +0800

    plugins: cxx lsp config && refactor.

--- lua/configs/lsp/index.lua
+++ lua/configs/lsp/index.lua
@@ -1,20 +1,11 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+-- cxx config
+require "configs.lsp.cxx"
 
--- EXAMPLE
-local servers = { "html", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
--- lua_ls config
+-- lua config
 require "configs.lsp.lua_ls"
+
+-- fontend config
+require "configs.lsp.frontend"
