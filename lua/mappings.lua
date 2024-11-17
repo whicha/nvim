f8e3e0f228a6224303e51e8c1b0f94395e0421a8
ao jia <ao_jia@outlook.com>
2024-11-17 13:57:53 +0800

    mappings: terminal and search related.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,36 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- >>> Telescope >>>
+
+map({ "n", "i", "v" }, "<C-f>", function()
+  if vim.bo.filetype == "TelescopePrompt" then
+    vim.cmd "q!"
+  else
+    vim.cmd "Telescope current_buffer_fuzzy_find"
+  end
+end, { desc = "search search in current buffer" })
+
+map({ "n", "i", "v" }, "<A-f>", function()
+  if vim.bo.filetype == "TelescopePrompt" then
+    vim.cmd "q!"
+  else
+    vim.cmd "Telescope live_grep"
+  end
+end, { desc = "search search across project" })
+
+-- >>> Telescope >>>
+
+-- >>> Terminal >>>
+
+map({ "n", "i", "v", "t" }, "<A-v>", function()
+  require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
+end, { desc = "terminal toggle vertical term" })
+map({ "n", "i", "v", "t" }, "<A-h>", function()
+  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+end, { desc = "terminal toggle horizontal term" })
+map({ "n", "i", "v", "t" }, "<A-i>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
+
+-- >>> Terminal >>>
