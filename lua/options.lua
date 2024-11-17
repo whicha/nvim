3ebbb16e68d8157f366f6a50357e6dcdb0e7e4e7
ao jia <ao_jia@outlook.com>
2024-11-17 14:07:28 +0800

    options: Enable OSC 52 for copying to system clipboard in SSH.

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,25 @@
 require "nvchad.options"
 
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+--- Enable OSC 52 for copying to system clipboard in SSH
+-- See: https://github.com/neovim/neovim/issues/28611#issuecomment-2147744670
+if os.getenv "SSH_TTY" then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    -- Try to use OSC 52 escape sequences to copy to system clipboard
+    -- It should not break anything if it’s not supported
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+      ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+    },
+    -- Disable paste since it does not work in some terminals,
+    -- i.e., xterm.js (many app based on Web tech use this)
+    paste = {
+      ["+"] = function(lines)
+        return vim.split(vim.fn.getreg '"', "\n")
+      end,
+      ["*"] = function(lines)
+        return vim.split(vim.fn.getreg '"', "\n")
+      end,
+    },
+  }
+end
