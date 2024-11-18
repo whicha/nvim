445a865dc4168518d71bd0ae85fb6a65cf500fca
ao jia <ao_jia@outlook.com>
2024-11-18 19:33:57 +0800

    plugins[outline]: add outline, a sidebar with a tree-like outline of symbols from your code, powered by LSP.

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "hedyhli/outline.nvim",
+  lazy = false,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = { -- Example mapping to toggle outline
+    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
+  opts = {
+    -- Your setup opts here
+  },
+}
